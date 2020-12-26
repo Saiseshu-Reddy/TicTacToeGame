3c23a5aca5ce667bc4a183b4698b19163ca74809
Saiseshu Reddy <saiseshureddy.isanaka@gmail.com>
2020-12-26 16:07:09 +0530

    Tic Tac Toe Game master branch

---
+++ TicTacToeGame.java
@@ -0,0 +1,14 @@
+public class TicTacToeGame {
+
+	public static void main(String[] args) {
+		char[] board = createBoard();
+	}
+
+	private  static char[] createBoard() {
+		char[] board = new char[10];
+		for(int i=0;i<board.length;i++) {
+			board[i]=' ';
+		}
+		return board;
+	}
+}
